e10f119d835dc7f0839d28635ec751aaccba71de
larisamgs <lary.mogos@gmail.com>
2022-06-09 10:10:15 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn react" />
+      <Todo text="Master react" />
+      <Todo text="Teach react" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Modal.js
@@ -0,0 +1,24 @@
+function Modal(props) {
+  function cancelHandler() {
+    props.onCancel();
+  }
+  function confirmHandler() {
+    props.onConfirm();
+  }
+
+  return (
+    <div className="modal">
+      <p>Are you sure?</p>
+      <button className="btn btn--alt" onClick={cancelHandler}>
+        {/* sau onclick={props.onCancel} */}
+        Cancel
+      </button>
+      <button className="btn" onClick={confirmHandler}>
+        {/* sau onclick={props.onConfirm} */}
+        Confirm
+      </button>
+    </div>
+  );
+}
+
+export default Modal;
